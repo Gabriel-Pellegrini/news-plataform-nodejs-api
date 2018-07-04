894833c592f11afd1c161062d4ebbe06f109328e
Gabriel Pellegrini <gpellegrinimaciel96@gmail.com>
2018-07-04 17:38:07 -0300

    connection with mysql database

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,6 +1,19 @@
+module.exports = function (app) {
 
-module.exports=function (app) {
     app.get("/noticias", function (req, res) {
-        res.render("noticias/noticias")
+        var mysql = require('mysql');
+
+        var connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: 'gabrieric',
+            database: 'mydatabase'
+        });
+
+        connection.query('select * from noticias', function (error, result) {
+            res.send(result);
+        })
+
+    //    res.render("noticias/noticias")
     });
 };
