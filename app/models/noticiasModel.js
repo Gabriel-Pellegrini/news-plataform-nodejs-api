f8497a7f030cc127ce859741773501e0f87ec7bc
Gabriel Pellegrini <gpellegrinimaciel96@gmail.com>
2018-07-09 20:34:29 -0300

    database insert implemented

--- app/models/noticiasModel.js
+++ app/models/noticiasModel.js
@@ -8,6 +8,10 @@ module.exports = function () {
         connection.query("select * from noticias where id_noticia = 3",callback);
     }
 
+    this.salvarNoticia = function (noticia,connection,callback) {
+        connection.query('insert into noticias set ?',noticia, callback)        
+    }
+
     return this;
     
 }
--- config/server.js
+++ config/server.js
@@ -1,10 +1,13 @@
 var express = require("express");
 var consign = require("consign");
 var app = express();
+var bodyParser = require("body-parser");
 
 // Definition of view engine and views directory
 app.set('view engine', 'ejs');
 app.set('views','./app/views');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 //Inclusion of directory routes to our projects
 consign()
