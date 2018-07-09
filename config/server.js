68e9e21754add750fa54fc71bc79c67598baf2f4
Gabriel Pellegrini <gpellegrinimaciel96@gmail.com>
2018-07-09 14:49:15 -0300

    first commit consign module inclusion

--- config/server.js
+++ config/server.js
@@ -1,8 +1,12 @@
 var express = require("express");
+var consign = require("consign");
 var app = express();
 
+// Definition of view engine and views directory
 app.set('view engine', 'ejs');
 app.set('views','./app/views');
 
+//Inclusion of directory routes to our projects
+consign().include('./app/routes').into(app);
 
 module.exports= app;
